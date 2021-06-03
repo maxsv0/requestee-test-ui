de8e765aea4e384cdf09cbd87117a6100ef0ad35
Maksym Svistunov <max.svistunov@gmail.com>
2021-06-03 13:25:26 +0200

    Add google-auth-library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuth2Client } from 'google-auth-library';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,16 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: OAuth2Client,
+      useValue: new OAuth2Client(
+        environment.gAPI.client_id,
+        environment.gAPI.client_secret,
+        environment.gAPI.redirect,
+      ),
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gAPI: {
+    client_id: '',
+    client_secret: '',
+    redirect: 'http://localhost:4200/home',
+  }
+
 };
 
 /*
